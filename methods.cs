c24257b3cf4f9757757dcd0078670b88c1bf15b4
Tim <monkeyhut0@gmail.com>
2026-09-04 16:07:13 -0400

    implements `AddOrUpdate()`

--- methods.cs
+++ methods.cs
@@ -17,18 +17,47 @@ public class CPHInline
         public string Prompt { get; set; }
 
         [JsonProperty("initialTime")]
-        public long InitialTime { get; set; }
+        public long InitialTime { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
         [JsonProperty("updateTime")]
-        public long UpdateTime { get; set; }
+        public long UpdateTime { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
         [JsonProperty("completed")]
-        public bool Completed { get; set; }
+        public bool Completed { get; set; } = false;
     }
 
-    public bool AddOrUpdate()
+    public bool AddOrUpdate(string userId, string color, string prompt)
     {
-        
+        // Using List instead of Queue so that we can check user positions and arbitrary removal
+        List<string> drawQueue = CPH.GetGlobalVar<List<string>>("drawQueue") ?? new List<string>();
+
+        if (drawQueue.Contains(userId))
+        {
+            // update existing request
+            DrawRequest existingRequest = CPH.GetTwtichUserVarById<DrawRequest>(userId, "drawRequest") ?? new DrawRequest();
+            existingRequest.Color = color;
+            existingRequest.Prompt = prompt;
+            existingRequest.UpdateTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            CPH.SetTwitchUserVarById(userId, existingRequest);
+        }
+        else
+        {
+            // add new request
+            DrawRequest newRequest = new DrawRequest
+            {
+                Color = color,
+                Prompt = prompt
+                // rest are default
+            };
+
+            CPH.SetTwitchUserVarById(userId, newRequest);
+
+            drawQueue.Add(userId);
+            CPH.SetGlobalVar("drawQueue", drawQueue);
+        }
+
+        BroadcastQueue();
     }
 
     public bool BroadcastQueue()
@@ -36,12 +65,28 @@ public class CPHInline
         
     }
 
-    public bool CompleteRequest()
+    public bool CompleteRequest(string userId)
     {
         
+
+        BroadcastQueue();
     }
 
-    public bool RejectRequest()
+    public bool RejectRequest(string userId)
+    {
+        
+
+        BroadcastQueue();
+    }
+
+    public bool ClearQueue()
+    {
+        
+
+        BroadcastQueue();
+    }
+
+    public bool PauseQueue()
     {
         
     }
