9672e3cced2f3f5220f455ef8571044e3c1d4134
Tim <monkeyhut0@gmail.com>
2026-09-05 23:49:57 -0400

    updates folder structure

---
+++ frontend/js/ws.js
@@ -0,0 +1,10 @@
+import { StreamerbotClient } from '@streamerbot/client';
+
+const client = new StreamerbotClient();
+
+// on connect log and get current queue
+
+// handle events
+function handleEvent(event) {
+
+}
